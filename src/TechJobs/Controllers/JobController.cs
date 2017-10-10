71d5c743a0789c27a30ffdd81f859b6cf12032f5
Jason <User@WINDEV1709EVAL>
2017-10-10 15:25:01 -0700

    Finished assigment: TechJob Object Oriented Edition

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TechJobs.Data;
+using TechJobs.Models;
 using TechJobs.ViewModels;
+using System;
 
 namespace TechJobs.Controllers
 {
@@ -20,7 +22,9 @@ namespace TechJobs.Controllers
         {
             // TODO #1 - get the Job with the given ID and pass it into the view
 
-            return View();
+            //JobFieldsViewModel jobDetail = new JobFieldsViewModel();
+            Job job = jobData.Find(id);
+            return View(job);
         }
 
         public IActionResult New()
@@ -35,6 +39,25 @@ namespace TechJobs.Controllers
             // TODO #6 - Validate the ViewModel and if valid, create a 
             // new Job and add it to the JobData data store. Then
             // redirect to the Job detail (Index) action/view for the new Job.
+
+            if (ModelState.IsValid)
+            {
+                Job newJob = new Job {
+                    // set properties
+                    Name = newJobViewModel.Name,
+                    Employer = jobData.Employers.Find(newJobViewModel.EmployerID),
+                    Location = jobData.Locations.Find(newJobViewModel.LocationID),
+                    CoreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.CoreCompetencyID),
+                    PositionType = jobData.PositionTypes.Find(newJobViewModel.PositionTypeID)
+                    
+                };
+
+                //Add a new job object to the list
+                jobData.Jobs.Add(newJob);
+
+
+                return Redirect("/Job?id="+newJob.ID);
+            }
 
             return View(newJobViewModel);
         }
